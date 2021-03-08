cd7a1802b61f458d0be5e951fa43d0b4f98206d6
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-03-08 02:32:53 -0600

    added aside section with list of cities for users to select from

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 // the get url
-var url = https://api.openweathermap.org/data/2.5/onecall?lat=;
+var url = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 
-// append to the url
-var userInput;
+// Grabs stuff in cities id
+var userCityId = document.getElementById('cities');
 
-url = url.append('')
+var userSelection = [ "Seatle", "New York", "New Orleans", "Los Angeles", "Austin"];
+
+console.log(userSelection.value);
+
+
+url = url + userSelection;
+
+function getSelectedOption(userSelection) {
+    var opt;
+    for ( var i = 0, len = userSelection.length; i < len; i++ ) {
+
+        opt = userSelection.options[i];
+        if ( opt.selected === true ) {
+            break;
+        }
+    }
+    return opt;
+}
+
+// get selected option in sel (reference obtained above)
+var opt = getSelectedOption(userSelection);
+
+// display its value and text
+console.log( opt.value );
+console.log( opt.text );
