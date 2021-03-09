1ec6a548d65fc0126cb5aaaf7d3992ca93d9a70a
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-03-08 22:21:22 -0600

    edited selection onclick event in js

--- script.js
+++ script.js
@@ -7,36 +7,44 @@ var userCityId = document.getElementById('cities');
 
 var el = document.getElementById('showVal');
 
-var userSelection = [ "Seatle", "New York", "New Orleans", "Los Angeles", "Austin"];
+var userSelection = ["Seatle", "New York", "New Orleans", "Los Angeles", "Austin"];
 
 // console.log(userSelection);
 
 
+
+// old code
 url = url + userSelection;
 
 function getSelectedOption(userSelection) {
     var opt;
-    for ( var i = 0, len = userSelection.length; i < len; i++ ) {
+    for (var i = 0, len = userSelection.length; i < len; i++) {
 
         opt = userSelection[i];
 
         console.log(opt);
-        
-        if ( opt.selected === true ) {
-            console.log(opt);
-            return opt;
-            
-            
+
+        if (opt.selected === true) {
+            // console.log(opt);
+            break;
+
+
         }
     }
     return opt;
 }
 
-// get selected option in sel (reference obtained above)
+
+
+// get selected option in userSelection (reference obtained above)
 var opt = getSelectedOption(userSelection);
 
 userCityId.onclick = function () {
-    el.value = userSelection.value; 
+    selection = window.getSelection();
+    localStorage.setItem("selected", selection);
+    el = selection.childNodes;
+    console.log(el);
+
 }
 
 
