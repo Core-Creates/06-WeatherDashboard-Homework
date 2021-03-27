47c4a196d767d8e497d71467ed674ba76562dfaf
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-03-27 17:40:46 -0500

    added google maps calls with JavaScript

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+
+
 $(document).ready(function () {
+
 
 
     document.addEventListener("DOMContentLoaded", function () {
         var elems = document.querySelectorAll("aside");
         // var instances = M.Sidenav.init(elems, options);
-      });
+    });
 
     // the get url
-    
+
     var url;
     var lat;
     var lon;
+    var googleMapKey = "AIzaSyCtFzdbJA-H72PJ4sIY0gGWT9mDwEloPkA";
 
     // Grabs stuff in cities id
     var userCityId = document.getElementById('cities');
@@ -22,17 +26,17 @@ $(document).ready(function () {
     // var userSelection = ["Seatle", "New York", "New Orleans", "Los Angeles", "Austin"];
 
 
-    userCityId.addEventListener('click', function(userSelection){
+    userCityId.addEventListener('click', function () {
 
         selection = window.getSelection();
         localStorage.setItem("selected", selection.anchorNode.data);
 
-        console.log(selection);
+        // console.log(selection);
 
         if (selection.anchorNode.data == "Seattle") {
 
-            lat = "47.606209";
-            lon =  "-122.332069";
+            lat = "47.606";
+            lon = "-122.332";
             time = "1586468027";
             API_key = "572c06f39e6d6617f9c6f6a00e8fe448";
             url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&dt=${time}&appid=${API_key}`;
@@ -43,29 +47,32 @@ $(document).ready(function () {
 
             $.ajax({
                 url: url,
-                dataType: "json", 
-                success: function(data) { 
+                dataType: "json",
+                success: function (data) {
                     currentTemperature = data.current.temp;
 
                     console.log("Current temp: " + currentTemperature);
                     city.innerHTML = `${selection.anchorNode.data} \n`;
 
-                    weather.innerHTML =  `\nCurrent Temperature: ${currentTemperature} °F \n`;
+                    weather.innerHTML = `\nCurrent Temperature: ${currentTemperature} °F `;
                     feelsLike.innerHTML = `\nFeels like: ${data.current.feels_like}`;
+                    initMap(lat, lon);
 
-                    
+
                 }
             });
 
 
-            console.log(url);
-            console.log(selection.anchorNode.data);
+
+
+            // console.log(url);
+            // console.log(selection.anchorNode.data);
 
         }
         else if (selection.anchorNode.data == "New York") {
 
             lat = "	40.730610";
-            lon =  "-73.935242";
+            lon = "-73.935242";
             time = "1586468027";
             API_key = "572c06f39e6d6617f9c6f6a00e8fe448";
             url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&dt=${time}&appid=${API_key}`;
@@ -76,29 +83,29 @@ $(document).ready(function () {
 
             $.ajax({
                 url: url,
-                dataType: "json", 
-                success: function(data) { 
+                dataType: "json",
+                success: function (data) {
                     currentTemperature = data.current.temp;
 
                     console.log("Current temp: " + currentTemperature);
                     city.innerHTML = `${selection.anchorNode.data} \n`;
 
-                    weather.innerHTML =  `\nCurrent Temperature: ${currentTemperature} °F \n`;
+                    weather.innerHTML = `\nCurrent Temperature: ${currentTemperature} °F `;
                     feelsLike.innerHTML = `\nFeels like: ${data.current.feels_like}`;
 
-                    
+
                 }
             });
 
-            
-            console.log(url);
-            console.log(selection.anchorNode.data);
+
+            // console.log(url);
+            // console.log(selection.anchorNode.data);
 
         }
         else if (selection.anchorNode.data == "New Orleans") {
 
             lat = "29.951065";
-            lon =  "-90.071533";
+            lon = "-90.071533";
             time = "1586468027";
             API_key = "572c06f39e6d6617f9c6f6a00e8fe448";
             url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&dt=${time}&appid=${API_key}`;
@@ -109,27 +116,27 @@ $(document).ready(function () {
 
             $.ajax({
                 url: url,
-                dataType: "json", 
-                success: function(data) { 
+                dataType: "json",
+                success: function (data) {
                     currentTemperature = data.current.temp;
 
                     console.log("Current temp: " + currentTemperature);
                     city.innerHTML = `${selection.anchorNode.data} \n`;
 
-                    weather.innerHTML =  `\nCurrent Temperature: ${currentTemperature} °F \n`;
+                    weather.innerHTML = `\nCurrent Temperature: ${currentTemperature} °F `;
                     feelsLike.innerHTML = `\nFeels like: ${data.current.feels_like}`;
 
-                    
+
                 }
             });
-  
-            console.log(url);
-            console.log(selection.anchorNode.data);
+
+            // console.log(url);
+            // console.log(selection.anchorNode.data);
         }
         else if (selection.anchorNode.data == "Los Angeles") {
 
             lat = "34.052235";
-            lon =  "-118.243683";
+            lon = "-118.243683";
             time = "1586468027";
             API_key = "572c06f39e6d6617f9c6f6a00e8fe448";
             url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&dt=${time}&appid=${API_key}`;
@@ -139,27 +146,27 @@ $(document).ready(function () {
 
             $.ajax({
                 url: url,
-                dataType: "json", 
-                success: function(data) { 
+                dataType: "json",
+                success: function (data) {
                     currentTemperature = data.current.temp;
 
                     console.log("Current temp: " + currentTemperature);
                     city.innerHTML = `${selection.anchorNode.data} \n`;
 
-                    weather.innerHTML =  `\nCurrent Temperature: ${currentTemperature} °F \n`;
+                    weather.innerHTML = `\nCurrent Temperature: ${currentTemperature} °F`;
                     feelsLike.innerHTML = `\nFeels like: ${data.current.feels_like}`;
 
                 }
             });
 
-            
+
             // console.log(url);
-            console.log(selection.anchorNode.data);
+            // console.log(selection.anchorNode.data);
         }
         else if (selection.anchorNode.data == "Austin") {
 
             lat = "	30.266666";
-            lon =  "-97.733330";
+            lon = "-97.733330";
             time = "1586468027";
             API_key = "572c06f39e6d6617f9c6f6a00e8fe448";
             url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&dt=${time}&appid=${API_key}`;
@@ -170,40 +177,57 @@ $(document).ready(function () {
 
             $.ajax({
                 url: url,
-                dataType: "json", 
-                success: function(data) { 
+                dataType: "json",
+                success: function (data) {
                     currentTemperature = data.current.temp;
 
                     console.log("Current temp: " + currentTemperature);
                     city.innerHTML = `${selection.anchorNode.data} \n`;
 
-                    weather.innerHTML =  `Current Temperature: ${currentTemperature} °F \n`;
+                    weather.innerHTML = `Current Temperature: ${currentTemperature} °F `;
                     feelsLike.innerHTML = `\nFeels like: ${data.current.feels_like}`;
 
-                    
+
                 }
             });
-            
-            
-            console.log(url);
-            console.log(selection.anchorNode.data);
+
+
+            // console.log(url);
+            // console.log(selection.anchorNode.data);
 
         }
         else {
-            
+
             console.log("not an option");
 
         }
 
         function loadDoc() {
             var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-              if (this.readyState == 4 && this.status == 200) {
-                ob = this.responseText;
-                
-              }
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    ob = this.responseText;
+
+                }
             }
         }
     })
 
 });
+
+let maps;
+
+function initMap(lat, lng) {
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        scaleControl: true,
+        center: { lat: +lat, lng: +lng},
+        zoom: 10,
+        
+    });
+    marker.addListener("click", () => {
+        infowindow.open(map, marker);
+      });
+    loadMapAt(new google.maps.LatLng(geo.lat, geo.lng));
+
+}
